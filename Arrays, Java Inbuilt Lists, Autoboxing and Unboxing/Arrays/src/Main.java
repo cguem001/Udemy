328fa197a7980022fc9e362afba3c63a64bfcab9
Carlos Guemes <carlosguemes.martinez@ynap.com>
2020-02-27 16:52:52 +0000

    Test

--- Arrays, Java Inbuilt Lists, Autoboxing and Unboxing/Arrays/src/Main.java
+++ Arrays, Java Inbuilt Lists, Autoboxing and Unboxing/Arrays/src/Main.java
@@ -8,7 +8,7 @@ public class Main {
         boolean quit = false;
         int choice = 0;
         printInstructions();
-        while(quit) {
+        while(!quit) {
             System.out.println("Enter your choice: ");
             choice = sc.nextInt();
             sc.nextLine();
@@ -41,13 +41,43 @@ public class Main {
 
     public static void printInstructions() {
         System.out.println("\nPress ");
-        System.out.println("\t ");
-        System.out.println("\t ");
-        System.out.println("\t ");
-        System.out.println("\t ");
-        System.out.println("\t ");
-        System.out.println("\t ");
-        System.out.println("\t ");
+        System.out.println("\t 0 - To print choice options");
+        System.out.println("\t 1 - To print the list of grocery items.");
+        System.out.println("\t 2 - To add an item to the list");
+        System.out.println("\t 3 - To modify an item of the list");
+        System.out.println("\t 4 - To remove an item from the list");
+        System.out.println("\t 5 - To search for an item int the list");
+        System.out.println("\t 6 - To quit the application");
+    }
 
+    public static void addItem() {
+        System.out.print("Please enter the grocery item: ");
+        groceryList.addGroceriesItem(sc.nextLine());
+    }
+
+    public static void modifyItem() {
+        System.out.print("Enter item number: ");
+        int itemNo = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Enter replacement item: ");
+        String newItem = sc.nextLine();
+        groceryList.modifyGroceryItem(itemNo-1, newItem);
+    }
+
+    public static void removeItem() {
+        System.out.print("Enter item number: ");
+        int itemNo = sc.nextInt();
+        sc.nextLine();
+        groceryList.removeGroceryItem(itemNo);
+    }
+
+    public static void searchForItem() {
+        System.out.println("Item to search for: ");
+        String searchItem = sc.nextLine();
+        if(groceryList.findItem(searchItem) != null) {
+            System.out.println("Found " + searchItem + " in our grocery List");
+        } else {
+            System.out.println(searchItem + " is not in the grocery List");
+        }
     }
 }
